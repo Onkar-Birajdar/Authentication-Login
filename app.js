ba617c4167d694eab4ae72330f6641e50ad80b69
Onkar Birajdar <onkarabirajdar@gmail.com>
2022-03-17 01:04:43 +0530

    Inital Setup app.js,port setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //jshint esversion:6
+const express= require("express");
+const bodyParser= require("body-parser");
+const mongoose= require("mongoose");
+const _= require("lodash");
+const ejs =require("ejs");
+const port=process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+// app.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
