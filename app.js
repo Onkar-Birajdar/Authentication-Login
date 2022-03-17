71087a44297806d1482c13b8f6df11308363167c
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-03-17 15:38:21 +0530

    Setting up database

--- app.js
+++ app.js
@@ -1,37 +1,68 @@
-//jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 const ejs = require("ejs");
 const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// app.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+app.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("user", userSchema);
+
 
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-
-
-
-
-
 app.get("/login", (req, res) => {
+    
     res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+    
+    const newUser = new user({
+        email: email,
+        password: password
+    });
+    newUser.save();
+    res.render("secrets");
+    console.log("New User created successfully");
 });
 
 app.post("/login", (req, res) => {
     const email = req.body.username;
     const password = req.body.password;
-
-    res.send("Got your response"+email+" "+password);
-    // const user = _.find(users, { email: email });
+    User.findOne({ email: email }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            } else {
+                res.send("No user found");
+            }
+        }
+    });
 });
 
 app.listen(port, () => {
