66962bc4b23e546ee8ba4a33f9e19e395c7ab0c4
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-03-25 14:01:51 +0530

    Added .env methods

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-var secret = "Thisisasecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = new mongoose.model("user", userSchema);
 
 
